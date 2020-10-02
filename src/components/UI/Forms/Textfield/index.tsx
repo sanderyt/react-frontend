6d333b33bc74eb24432ec5394a1fe0bb8b1fbc27
Sander van Rijsoort <hallo@svanrijsoort.nl>
2020-10-03 00:30:44 +0200

    finished styling for input

--- src/components/UI/Forms/Textfield/index.tsx
+++ src/components/UI/Forms/Textfield/index.tsx
@@ -10,17 +10,25 @@ type TextfieldProps = {
 };
 
 const Textfield: FC<TextfieldProps> = ({ label, type, register, error }) => {
+  const capitalize = (word: string): string => {
+    return word.charAt(0).toUpperCase() + word.substring(1);
+  };
+
   return (
     <div className="textfield">
       <input
         name={label}
         type={type}
         ref={register}
-        className="textfield__input"
+        className={
+          error
+            ? "textfield__input textfield__input--error"
+            : "textfield__input"
+        }
         required={true}
       />
       <label htmlFor={label} className="textfield__label">
-        {label}
+        {capitalize(label)}
       </label>
       {error && <span className="textfield__error">{error.message}</span>}
     </div>
